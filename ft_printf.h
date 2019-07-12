cef5e0a0e724f6721017fa44c9b677b049aab43e
Ina Botnaru <ibotnaru@e1z3r7p6.42.us.org>
2019-07-11 21:26:09 -0700

    in_progres

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ibotnaru <ibotnaru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/17 13:46:57 by ibotnaru          #+#    #+#             */
-/*   Updated: 2019/07/11 20:26:28 by ibotnaru         ###   ########.fr       */
+/*   Updated: 2019/07/11 21:25:32 by ibotnaru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,22 +34,22 @@ typedef struct s_flags
     int     hh_flag;
 }   t_flags;
 
-int		ft_printf(const char *format, ...);
-int		ft_print_c(va_list ap, t_flags *all_flags);
-int		ft_print_s(va_list ap, t_flags *all_flags);
-void	get_the_flag(const char *format, t_flags *all_flags);
-void	get_and_store_width(const char *format, t_flags *all_flags);
-void	get_and_store_precision(const char *format, t_flags *all_flags);
-void	get_and_store_flag_hash(const char *format, t_flags *all_flags);
-void	get_and_store_flag_zero(const char *format, t_flags *all_flags);
-void	get_and_store_flag_minus(const char *format, t_flags *all_flags);
-void	get_and_store_flag_plus(const char *format, t_flags *all_flags);
-void	get_and_store_flag_space(const char *format, t_flags *all_flags);
-void	get_and_store_flags_l_ll(const char *format, t_flags *all_flags);
-void	get_and_store_flags_h_hh(const char *format, t_flags *all_flags);
+int     ft_printf(const char *format, ...);
+int     ft_print_c(va_list ap, t_flags *all_flags);
+int     ft_print_s(va_list ap, t_flags *all_flags);
+void    get_the_flag(const char *format, t_flags *all_flags);
+void    get_and_store_width(const char *format, t_flags *all_flags);
+void    get_and_store_precision(const char *format, t_flags *all_flags);
+void    get_and_store_flag_hash(const char *format, t_flags *all_flags);
+void    get_and_store_flag_zero(const char *format, t_flags *all_flags);
+void    get_and_store_flag_minus(const char *format, t_flags *all_flags);
+void    get_and_store_flag_plus(const char *format, t_flags *all_flags);
+void    get_and_store_flag_space(const char *format, t_flags *all_flags);
+void    get_and_store_flags_l_ll(const char *format, t_flags *all_flags);
+void    get_and_store_flags_h_hh(const char *format, t_flags *all_flags);
 void    find_specifier(t_flags *all_flags, const char *format, int i);
-char	*put_buffer(va_list ap, t_flags *all_flags);
-int		ft_print_buffer(char	*buffer);
+char    *put_buffer(va_list ap, t_flags *all_flags);
+int     ft_print_buffer(char	*buffer);
 char    *put_zeroes_precision(t_flags *all_flags, int num_len);
 char    *put_zeroes_flag(t_flags *all_flags, int num_len);
 char    *put_sign(char  sign);
