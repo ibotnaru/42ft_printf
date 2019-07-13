e9bf7e4059a9668e3c50b969df343daaf6a7bfea
Ina Botnaru <ibotnaru@e1z3r3p3.42.us.org>
2019-07-12 21:21:24 -0700

    in_progress

--- print_d_i.c
+++ print_d_i.c
@@ -6,7 +6,7 @@
 /*   By: ibotnaru <ibotnaru@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 17:15:39 by ibotnaru          #+#    #+#             */
-/*   Updated: 2019/07/11 21:16:07 by ibotnaru         ###   ########.fr       */
+/*   Updated: 2019/07/12 21:19:30 by ibotnaru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,6 @@ static intmax_t	get_d_number(va_list ap, t_flags *all_flags)
 	return (number);
 }
 
-
-//for positive integer only
 //handles only one flag at the time
 char        *buffer_d(va_list ap, t_flags *all_flags)
 {
@@ -45,46 +43,94 @@ char        *buffer_d(va_list ap, t_flags *all_flags)
 	width = all_flags->width_size;
 	prec = all_flags->precision_size;
 	number = get_d_number(ap, all_flags);
-	str_num = ft_itoa(number);
-	num_len = ft_strlen(str_num);
-	if ((all_flags->width_flag == 1 && all_flags->precision_flag == 1) && (width > num_len && width > num_len) && (width > prec))
+	if (number >= 0) //for positive integer only
 	{
-		buffer = ft_strjoin(put_zeroes_precision(all_flags, num_len), str_num);
-		if (all_flags->minus_flag == 1)
-			buffer = ft_strjoin_free(buffer, put_spaces_width(all_flags, ft_strlen(buffer)), 1);
-		else if (all_flags->plus_flag == 1)
+		str_num = ft_itoa(number);
+		num_len = ft_strlen(str_num);
+		if ((all_flags->width_flag == 1 && all_flags->precision_flag == 1) && (width > num_len && width > num_len) && (width > prec))
 		{
-			buffer = ft_strjoin("+", buffer);
-			buffer = ft_strjoin_free(put_spaces_width(all_flags, ft_strlen(buffer)), buffer, 2);
+			buffer = ft_strjoin(put_zeroes_precision(all_flags, num_len), str_num);
+			if (all_flags->minus_flag == 1)
+				buffer = ft_strjoin_free(buffer, put_spaces_width(all_flags, ft_strlen(buffer)), 1);
+			else if (all_flags->plus_flag == 1)
+			{
+				buffer = ft_strjoin("+", buffer);
+				buffer = ft_strjoin_free(put_spaces_width(all_flags, ft_strlen(buffer)), buffer, 2);
+			}
+			else
+				buffer = ft_strjoin_free(put_spaces_width(all_flags, ft_strlen(buffer)), buffer, 2);
 		}
-		else
-			buffer = ft_strjoin_free(put_spaces_width(all_flags, ft_strlen(buffer)), buffer, 2);
-	}
-	else if ((all_flags->width_flag == 1 && all_flags->precision_flag == 0) && all_flags->width_size > num_len)
-	{
-		if (all_flags->minus_flag == 1)
-			buffer = ft_strjoin(str_num, put_spaces_width(all_flags, num_len));
-		else if (all_flags->plus_flag == 1)
+		else if ((all_flags->width_flag == 1 && all_flags->precision_flag == 0) && all_flags->width_size > num_len)
 		{
+			if (all_flags->minus_flag == 1)
+				buffer = ft_strjoin(str_num, put_spaces_width(all_flags, num_len));
+			else if (all_flags->plus_flag == 1)
+			{
+				buffer = ft_strjoin("+", str_num);
+				buffer = ft_strjoin_free(put_spaces_width(all_flags, ft_strlen(buffer)), buffer, 2);
+			}
+			else if (all_flags->zero_flag == 1)
+				buffer = ft_strjoin(put_zeroes_flag(all_flags, num_len),str_num);
+			else
+				buffer = ft_strjoin(put_spaces_width(all_flags, num_len),str_num);
+		}
+		else if (((all_flags->width_flag == 0 && all_flags->precision_flag == 1) && prec > num_len) || ((all_flags->width_flag == 1 &&
+				all_flags->precision_flag == 1) && (width < prec) && ((width > num_len) && prec > num_len)))
+		{
+			buffer = ft_strjoin(put_zeroes_precision(all_flags, ft_strlen(str_num)),str_num);
+			if (all_flags->plus_flag == 1)
+				buffer = ft_strjoin_free("+", buffer, 2);
+			else if (all_flags->space_flag == 1)
+				buffer = ft_strjoin_free(" ", buffer, 2);
+		}
+		else if (all_flags->plus_flag == 1)
 			buffer = ft_strjoin("+", str_num);
-			buffer = ft_strjoin_free(put_spaces_width(all_flags, ft_strlen(buffer)), buffer, 2);
-		}
-		else if (all_flags->zero_flag == 1)
-			buffer = ft_strjoin(put_zeroes_flag(all_flags, num_len),str_num);
-		else
-			buffer = ft_strjoin(put_spaces_width(all_flags, num_len),str_num);
-	}
-	else if (((all_flags->width_flag == 0 && all_flags->precision_flag == 1) && prec > num_len) || ((all_flags->width_flag == 1 &&
-			all_flags->precision_flag == 1) && (width > prec) && ((width > num_len) && prec > num_len)))
-	{
-		buffer = ft_strjoin(put_zeroes_precision(all_flags, ft_strlen(str_num)),str_num);
-		if (all_flags->plus_flag == 1)
-			buffer = ft_strjoin_free("+", buffer, 2);
 		else if (all_flags->space_flag == 1)
-			buffer = ft_strjoin_free(" ", buffer, 2);
+			buffer = ft_strjoin(" ", str_num);
+		else
+			buffer = ft_strdup(str_num);
+		return (buffer);
 	}
-	else
-		buffer = ft_strdup(str_num);
-	return (buffer);
+	else	//if number is negative
+	{
+		str_num = ft_itoa_negative(number);
+		num_len = ft_strlen(str_num);
+		if ((all_flags->width_flag == 1 && all_flags->precision_flag == 1) && (width > num_len && width > num_len) && (width > prec))
+		{
+			buffer = ft_strjoin(put_zeroes_precision(all_flags, num_len), str_num);
+			buffer = ft_strjoin_free("-", buffer, 2);
+			if(all_flags->minus_flag == 1)
+				buffer = ft_strjoin_free(buffer, put_spaces_width(all_flags, ft_strlen(buffer)), 1);
+			else
+				buffer = ft_strjoin_free(put_spaces_width(all_flags, ft_strlen(buffer)), buffer, 2);
+		}
+		else if ((all_flags->width_flag == 1 && all_flags->precision_flag == 0) && all_flags->width_size > num_len)
+		{
+			if (all_flags->zero_flag == 1)
+			{
+				buffer = ft_strjoin(put_zeroes_flag(all_flags, num_len), str_num);
+				buffer = ft_strjoin_free("-", buffer, 2);
+			}
+			else if (all_flags->minus_flag == 1)
+			{
+				buffer = ft_strjoin("-", str_num);
+				buffer = ft_strjoin_free(buffer, put_spaces_width(all_flags, ft_strlen(buffer)), 1);
+			}
+			else
+			{
+				buffer = ft_strjoin("-", str_num);
+				buffer = ft_strjoin_free(put_spaces_width(all_flags, ft_strlen(buffer)), buffer, 2);
+			}
+		}
+		else if (((all_flags->width_flag == 0 && all_flags->precision_flag == 1) && prec > num_len) || ((all_flags->width_flag == 1 &&
+				all_flags->precision_flag == 1) && (width < prec) && ((width > num_len) && prec > num_len)))
+		{
+			buffer = ft_strjoin(put_zeroes_precision(all_flags, ft_strlen(str_num)),str_num);
+			buffer = ft_strjoin_free("-", buffer, 2);
+		}
+		else
+			buffer = ft_strjoin("-", str_num);
+		return (buffer);
+	}
 }
 
